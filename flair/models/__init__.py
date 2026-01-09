acff9d38699694c94a91efd903c7009ecf2f696a
Alexander Reinicke <167530118+areinicke@users.noreply.github.com>
2026-01-09 11:53:34 +0100

    Adding support for new Dual Encoder class

--- flair/models/__init__.py
+++ flair/models/__init__.py
@@ -13,7 +13,7 @@ from .tars_model import FewshotClassifier, TARSClassifier, TARSTagger
 from .text_classification_model import TextClassifier
 from .text_regression_model import TextRegressor
 from .word_tagger_model import TokenClassifier, WordTagger
-from .dual_encoder_ED import DualEncoderEntityDisambiguation, GreedyDualEncoderEntityDisambiguation
+from .dual_encoder_ED import DualEncoderEntityDisambiguation, GreedyDualEncoderEntityDisambiguation, LLMDualEncoderEntityDisambiguation
 #from .dual_encoder_ED_frozen_v1 import DualEncoderEntityDisambiguation as DualEncoderEntityDisambiguation_frozen_v1, GreedyDualEncoderEntityDisambiguation as GreedyDualEncoderEntityDisambiguation_frozen_v1 # was added for stable evaluation during implementation, remove this later!
 
 
@@ -38,6 +38,7 @@ __all__ = [
     "MultitaskModel",
     "DualEncoderEntityDisambiguation",
     "GreedyDualEncoderEntityDisambiguation",
+    "LLMDualEncoderEntityDisambiguation",
     #"DualEncoderEntityDisambiguation_frozen_v1",
     #"GreedyDualEncoderEntityDisambiguation_frozen_v1",
 ]
